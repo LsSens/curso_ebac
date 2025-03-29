4ebb39e7d38144a2d9a7d2bef5721ddee333c1a2
Lucas Sens <lucassousasens@gmail.com>
2025-03-28 21:23:06 -0300

    fix: remove support file

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     video: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    supportFile: false,
   },
 });
